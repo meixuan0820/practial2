0b94d5c9594e9ab12870284c2218900cecbc8d89
meixuan0820 <a1692978@adelaide.edu.au>
2024-02-10 08:56:50 +0000

    file16 changed

--- question16.sh
+++ question16.sh
@@ -1,10 +1,8 @@
-git fetch --all
+cd git-practice-02
 git checkout branch2
-git merge branch3
-git commit -m "Merge branch3 into branch2"
+git merge origin/branch3
 git add .
 git commit -m "Merge branch3 into branch2"
-git branch -d branch3
+git branch -D branch3
 git add .
 git commit -m "b3 deleted"
-git push
